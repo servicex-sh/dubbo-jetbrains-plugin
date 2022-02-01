9f1438486184b7ed3b976bff971f6888cbdf176e
linux_china <libing.chen@gmail.com>
2022-01-31 16:00:20 -0800

    DubboRequestExecutionSupport

---
+++ src/main/kotlin/org/jetbrains/plugins/dubbo/restClient/execution/DubboRequestExecutionSupport.kt
@@ -0,0 +1,26 @@
+package org.jetbrains.plugins.dubbo.restClient.execution
+
+import com.intellij.httpClient.execution.common.RequestContext
+import com.intellij.httpClient.execution.common.RequestConverter
+import com.intellij.httpClient.execution.common.RequestExecutionSupport
+import com.intellij.httpClient.execution.common.RequestHandler
+
+@Suppress("UnstableApiUsage")
+class DubboRequestExecutionSupport : RequestExecutionSupport<DubboRequest> {
+
+    override fun canProcess(requestContext: RequestContext): Boolean {
+        return requestContext.method == "DUBBO"
+    }
+
+    override fun getRequestConverter(): RequestConverter<DubboRequest> {
+        return DubboRequestConverter()
+    }
+
+    override fun getRequestHandler(): RequestHandler<DubboRequest> {
+        return DubboRequestHandler()
+    }
+
+    override fun supportedMethods(): Collection<String> {
+        return listOf("DUBBO")
+    }
+}
