8d9b131a8ab6bafafdca801b66ea6f335b5c5f67
linux_china <libing.chen@gmail.com>
2022-01-31 16:10:33 -0800

    DubboUrlTargetInfo

---
+++ src/main/kotlin/org/jetbrains/plugins/dubbo/endpoints/DubboUrlTargetInfo.kt
@@ -0,0 +1,25 @@
+package org.jetbrains.plugins.dubbo.endpoints
+
+import com.intellij.microservices.url.Authority
+import com.intellij.microservices.url.UrlPath
+import com.intellij.microservices.url.UrlTargetInfo
+import com.intellij.psi.PsiElement
+
+
+@Suppress("UnstableApiUsage")
+class DubboUrlTargetInfo(private val element: PsiElement, private val routing: String) : UrlTargetInfo {
+    override val authorities: List<Authority>
+        get() = emptyList()
+    override val path: UrlPath
+        get() = UrlPath.fromExactString(routing)
+    override val schemes: List<String>
+        get() = listOf("dubbo", "tcp")
+
+    override fun resolveToPsiElement() = element
+
+    override val methods: Set<String>
+        get() = setOf("DUBBO")
+
+    override val documentationPsiElement: PsiElement
+        get() = element
+}
