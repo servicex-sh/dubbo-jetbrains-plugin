74f0ad612341e561d064d3c6a9dd8a5cdc73be32
linux_china <libing.chen@gmail.com>
2022-02-08 16:44:01 -0800

    DubboUrlResolverFactory

---
+++ src/main/kotlin/org/jetbrains/plugins/dubbo/restClient/url/DubboUrlResolverFactory.kt
@@ -0,0 +1,54 @@
+@file:Suppress("UnstableApiUsage")
+
+package org.jetbrains.plugins.dubbo.restClient.url
+
+import com.intellij.microservices.url.*
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.dubbo.file.DubboServiceFileIndex
+
+
+class DubboUrlResolverFactory : UrlResolverFactory {
+    override fun forProject(project: Project): UrlResolver {
+        return DubboUrlResolver(project)
+    }
+}
+
+class DubboUrlResolver(private val project: Project) : UrlResolver {
+    override val authorityHints: List<Authority.Exact> = listOf()
+    override val supportedSchemes: List<String>
+        get() {
+            return listOf("dubbo://")
+        }
+
+    override fun getVariants(): Iterable<UrlTargetInfo> {
+        return listOf()
+    }
+
+    override fun resolve(request: UrlResolveRequest): Iterable<UrlTargetInfo> {
+        return listOf(RSocketUrlTargetInfo(project, request.X))
+    }
+
+}
+
+class RSocketUrlTargetInfo(private val project: Project, private val urlPath: UrlPath) : UrlTargetInfo {
+    override val authorities: List<Authority> = listOf()
+    override val path: UrlPath = urlPath
+    override val schemes: List<String> = listOf("dubbo")
+
+    override val methods: Set<String> = setOf("DUBBO")
+
+    override fun resolveToPsiElement(): PsiElement? {
+        var dubboRouting = urlPath.getPresentation()
+        if (dubboRouting.indexOf('/') > 8) {
+            dubboRouting = dubboRouting.substring(dubboRouting.indexOf('/', 8) + 1)
+        }
+        if (dubboRouting.contains('?')) {
+            dubboRouting = dubboRouting.substring(0, dubboRouting.indexOf('?'))
+        }
+        val parts = dubboRouting.split('/')
+        return DubboServiceFileIndex.findRelatedElement(project, parts[0], parts[1])
+    }
+
+}
+
