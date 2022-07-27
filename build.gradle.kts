daa40e9504de53a6cc6be3e0d1d818d275f76612
linux_china <libing.chen@gmail.com>
2022-07-27 15:36:37 -0700

    build: update to org.jetbrains.intellij 1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.5.3"
+    id("org.jetbrains.intellij") version "1.7.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
 }
@@ -33,7 +33,7 @@ intellij {
 }
 
 dependencies {
-    implementation("com.caucho:hessian:4.0.65")
+    implementation("com.caucho:hessian:4.0.66")
 }
 
 configurations.implementation {
