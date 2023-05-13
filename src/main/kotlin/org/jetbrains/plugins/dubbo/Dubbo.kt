f4c3d1dfe150f986c2c75cc034c100392590e54a
Libing Chen <libing.chen@gmail.com>
2023-05-13 10:55:42 +0800

    chore: polishing

--- src/main/kotlin/org/jetbrains/plugins/dubbo/Dubbo.kt
+++ src/main/kotlin/org/jetbrains/plugins/dubbo/Dubbo.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.dubbo
 
 import com.intellij.openapi.util.IconLoader
+import org.jetbrains.plugins.dubbo.psi.DubboService
 
-val dubboIcon = IconLoader.findIcon("dubbo-16x16.png")!!
+val dubboIcon = IconLoader.findIcon("dubbo-16x16.png", DubboService::class.java )!!
 var dubboServiceAnnotationName = "org.apache.dubbo.config.annotation.DubboService"
